506a76f31b0b5fc47d074308b86c9ab432731571
Abhinay2004-Debug <sai2004abhi@gmail.com>
2026-02-23 20:39:48 +0530

    Update till question 20 from question 13

--- Assignmentcodes.py
+++ Assignmentcodes.py
@@ -366,3 +366,406 @@ for i in range(1, n + 1):
         break
     
     print() 
+
+# Question 12:  Multiplication Table Generator
+# Take a number as input and generate its multiplication table up to given range using a loop.
+num = int(input("Enter a number"))
+range_limit = int(input("Enter range limit:"))
+
+for i in range(1,range_limit+1):
+    print(f"{num} x {i} = {num*i}")
+
+
+# Question 13:  Sum and Average Calculator
+#Take a list of numbers as input and compute simple sum , average of those numbers , find largest and smallest numbers using for loop and conditional functions. 
+count = int(input("Enter count of numbers: "))
+
+
+if count <= 0:
+    print("Invalid count")
+else:
+    first = float(input("Enter number 1: "))
+    total = first
+    maximum = first
+    minimum = first
+
+    for i in range(2, count + 1):
+        num = float(input(f"Enter number {i}: "))
+        total += num
+
+        if num > maximum:
+            maximum = num
+        if num < minimum:
+            minimum = num
+
+    average = total / count
+
+    print(f"Sum: {total}")
+    print(f"Average: {average}")
+    print(f"Maximum: {maximum}")
+    print(f"Minimum: {minimum}")  
+
+
+#Question 14: Factorial Calculator
+# Take a number as input and calculate its factorial using a loop.
+num = int(input("Enter a number: "))
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+elif num == 0 or num == 1:
+    print(f" {num}! = 1.")
+else:
+    factorial = 1
+    expression = ""
+    for i in range(1, num + 1):
+        factorial *= i
+        expression += str(i)
+        if i != num:
+            expression += " x "# Trailing x for all numbers except the last one to create the expression string
+    
+    print(f"{num}! = {expression} = {factorial}")
+
+
+# Question 15: Prime Number Checker
+# Simple condition of modulus will help to check if it is prime or not.Here if modulus is 0 then it is not prime else it is prime.
+
+num = int(input("Enter a number: "))
+if num <= 1:
+    print(num, "is not a prime number.")
+elif num == 2:
+    print(num, "is a prime number.")
+
+else:
+    is_prime = True
+    for i in range(2,num):
+        if num%i == 0:
+            is_prime = False
+            break   
+    if is_prime:
+        print(num, "is a prime number.")    
+    else:
+        print(num, "is not a prime number.")
+
+start = int(input("Enter start range: "))
+end = int(input("Enter end range: "))
+
+primes = []   # Array to store prime numbers
+
+for num in range(start, end + 1):
+
+    if num > 1:
+        is_prime = True
+
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes.append(num)
+
+print("Prime numbers:", end=" ")
+for p in primes:
+    print(p, end="  ")
+
+# Question 16: Random Number Guessing Game
+import random
+
+while True:
+    print("\n    NUMBER GUESSING GAME \n")
+    
+    secret_number = random.randint(1, 100)
+    attempts = 7
+    guessed_correctly = False
+
+    print(" A number between 1 and 100 has been randomly selected.")
+    print("You have 7 attempts to guess it.")
+
+    while attempts > 0:
+        guess = int(input("\nEnter your guess: "))
+
+        if guess == secret_number:
+            print(f" Congratulations! You guessed it in {8 - attempts} attempts.")
+            guessed_correctly = True
+            break
+
+        elif guess < secret_number:
+            attempts -= 1
+            print("Too low!")
+            print(f"Attempts remaining: {attempts}")
+
+        else:
+            attempts -= 1
+            print("Too high!")
+            print(f"Attempts remaining: {attempts}")
+
+    if not guessed_correctly:
+        print(f"\nYou failed! The correct number was {secret_number}.")
+
+    # Play again option
+    play_again = input("\nDo you want to play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
+
+
+    # Question 17:  Palindrome Checker
+# The original string is converted to lowercase to ensure the palindrome check is case-insensitive. Then, a loop iterates through the string in reverse order to construct the reversed version. Finally, the original and reversed strings are compared to determine if it's a palindrome or not.
+
+value = input("Enter a string or number: ")
+original = value
+process = original.lower()
+reversed_value = ""
+
+for i in range(len(process)-1,-1,-1):
+    reversed_value += process[i]
+    
+print("Original:", original)
+print("Reversed:", reversed_value)  
+
+if process == reversed_value:
+    print("Result: Palindrome") 
+else:
+    print("Result: Not a palindrome")
+
+
+   # Question 18: Calculator with Functions
+# Here u create functions for each arithmetic operation (addition, subtraction, multiplication, division, modulus, and exponentiation) where simple formula is enclosed in each function. Then you create a menu-driven interface that allows the user to select an operation and input the required numbers. The program calls the appropriate function based on the user's choice and displays the result in a formatted way. A loop is used to allow multiple calculations until the user chooses to exit.
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero is not allowed."
+    
+def modulus(a, b):
+    if b != 0:
+        return a % b
+    else:
+        return "Error: Modulus by zero is not allowed."
+    
+def exponentiate(a, b):
+    return a ** b
+
+def calculator():
+    while True:
+        print("\n===== CALCULATOR =====")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Modulus")
+        print("6. Power")
+        print("7. Exit")
+
+        choice = input("Enter your choice (1-7): ")
+
+        if choice == "7":
+            print("Exiting Calculator")
+            break
+
+        if choice in ["1", "2", "3", "4", "5", "6"]:
+            a = float(input("Enter first number: "))
+            b = float(input("Enter second number: "))
+
+            if choice == "1":
+                print("Result:", add(a, b))
+
+            elif choice == "2":
+                print("Result:", subtract(a, b))
+
+            elif choice == "3":
+                print("Result:", multiply(a, b))
+
+            elif choice == "4":
+                print("Result:", divide(a, b))
+
+            elif choice == "5":
+                print("Result:", modulus(a, b))
+
+            elif choice == "6":
+                print("Result:", exponentiate(a, b))
+
+        else:
+            print("Invalid choice. Please select 1-7.")
+
+
+# Run the calculator
+calculator()
+
+
+#  Question 20: Number System Functions
+
+# 1. Factorial
+def fact(n):
+    if n < 0:
+        return "Invalid input "
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+# 2. Prime Check
+def is_prim(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+# 3. Fibonacci (nth term)
+def fibonacci(n):
+    if n < 0:
+        return "Invalid input"
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a = 0
+    b = 1
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
+
+
+# 4. Sum of Digits
+def sum_of_digits(n):
+    n = abs(n)
+    total = 0
+    while n > 0:
+        total += n % 10
+        n //= 10
+    return total
+
+
+# 5. Reverse Number
+def reverse_number(n):
+    sign = -1 if n < 0 else 1
+    n = abs(n)
+    reverse = 0
+    while n > 0:
+        reverse = reverse * 10 + (n % 10)
+        n //= 10
+    return sign * reverse
+
+
+# 6. Armstrong Number
+def is_armstrong(n):
+    temp = abs(n)
+    digits = len(str(temp))
+    total = 0
+    original = temp
+
+    while temp > 0:
+        digit = temp % 10
+        total += digit ** digits
+        temp //= 10
+
+    return total == original
+
+
+# 7. GCD (Euclidean Algorithm)
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return abs(a)
+
+
+# 8. LCM
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+
+# 9. Perfect Number
+def is_perfect_number(g):
+    if g <= 1:
+        return False
+
+    total = 0
+    for i in range(1, g):
+
+        if g % i == 0:
+            total += i
+
+    return total == g
+
+
+# 10. Menu
+def math_menu():
+    while True:
+        print("\n            NUMBER SYSTEM MENU       \n")
+        print("1. Factorial")
+        print("2. Prime Check")
+        print("3. Fibonacci")
+        print("4. Sum of Digits")
+        print("5. Reverse Number")
+        print("6. Armstrong Number")
+        print("7. GCD")
+        print("8. LCM")
+        print("9. Perfect Number")
+        print("10. Exit")
+
+        choice = input("Enter your choice (1-10): ")
+
+        if choice == "10":
+            print("Exiting Program.")
+            break
+
+        elif choice == "1":
+            n = int(input("Enter number: "))
+            print("Result:", fact(n))
+
+        elif choice == "2":
+            n = int(input("Enter number: "))
+            print( is_prim(n))
+
+        elif choice == "3":
+            n = int(input("Enter n: "))
+            print("Fibonacci term:", fibonacci(n))
+
+        elif choice == "4":
+            n = int(input("Enter number: "))
+            print("Sum of digits:", sum_of_digits(n))
+
+        elif choice == "5":
+            n = int(input("Enter number: "))
+            print("Reversed number:", reverse_number(n))
+
+        elif choice == "6":
+            n = int(input("Enter number: "))
+            print( is_armstrong(n))
+
+        elif choice == "7":
+            a = int(input("Enter first number: "))
+            b = int(input("Enter second number: "))
+            print("GCD:", gcd(a, b))
+
+        elif choice == "8":
+            a = int(input("Enter first number: "))
+            b = int(input("Enter second number: "))
+            print("LCM:", lcm(a, b))
+
+        elif choice == "9":
+            n = int(input("Enter number: "))
+            print("Perfect number?", is_perfect_number(n))
+
+        else:
+            print("Invalid choice. Please try again.")
+
+
+# Main program
+math_menu()
